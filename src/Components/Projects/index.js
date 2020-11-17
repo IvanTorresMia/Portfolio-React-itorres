53d6999f9113387c7241b8098d494352bf10199e
Ivan Torres <ivantorresmia27@gmail.com>
2020-11-16 23:16:34 -0600

    made the json file

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -5,10 +5,11 @@ import ProjectsContext from '../../utils/ProjectsContext'
 function Projects() {
 const projects = useContext(ProjectsContext)
     return (
-        <div className="jumbotron style">
+        <div className="jumbotron Style">
             <div className="row">
-                
+            <h1 className="display-4">Check out my projects</h1>
             </div>
+            <hr />
             <div className="row">
 
             </div>
